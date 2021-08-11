f973e527eb078445a96ed4d456de6953faf4dfc1
Lucas Vasconcelos <eng_cadu@hotmail.com>
2021-08-11 16:49:01 -0300

    fix: add JPA implementation for SubjectPersistenceAdapter
    Problem:
    The application didn't start because spring could
    not autowire the beans for JPA components. JPA
    components was missing.
    
    Solution:
    Create JPA implementation the subject persistence adpater.
    - Implement the SubjectJpaAdapter interface
    - Create a Subject JPA entity
    - Create the Subject repository
    - Create a mapper to convert Core Entity to JPA Entity
    
    In order to keep the core application decoupled, it was
    created JPA entities which will be used in JPA operations.
    A mapper was criated to convert Core Entity into
    JPA Entity and vice versa.
    
    Obs:
    Still need to create the remaining JPA componentes.

---
+++ src/main/java/br/com/lucasv/southsystem/assembly/infra/persistence/jpa/entity/SubjectJpa.java
@@ -0,0 +1,61 @@
+package br.com.lucasv.southsystem.assembly.infra.persistence.jpa.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * <p>JPA entity to store a Subject.
+ * 
+ * <p>Because the Core Entities of the application is isolated
+ * from the persistence layer, it is necessary to crate
+ * JPA entities wrappers to store the data of the Core Entities.
+ * 
+ * <p>The information about the fields can be found in
+ * {@link br.com.lucasv.southsystem.assembly.core.entity.Subject}.
+ * 
+ * @author Lucas Vasconcelos 
+ * @see br.com.lucasv.southsystem.assembly.core.entity.Subject
+ *
+ */
+@Entity(name="Subject")
+public class SubjectJpa {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  
+  private String description;
+  
+  public SubjectJpa() {
+  }
+  
+  public SubjectJpa(Integer id, String description) {
+    super();
+    this.id = id;
+    this.description = description;
+  }
+
+  public SubjectJpa(String description) {
+    super();
+    this.description = description;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+  
+}
